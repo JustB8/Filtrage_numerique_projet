0e0eb0f92a2b1d51f3992d510d92c51de84fea71
nolan-rousseau <nolanr.contact@gmail.com>
2026-05-06 12:17:23 +0200

    Création du venv

---
+++ test_install.py
@@ -0,0 +1,7 @@
+import customtkinter as ctk
+import scipy.signal as signal
+import sounddevice as sd
+import numpy as np
+
+print("Installation réussie !")
+print(f"Version de CustomTkinter : {ctk.__version__}")
